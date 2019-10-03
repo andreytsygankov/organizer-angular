b5f37b4594354c62d7e8ab2c33cf7f3147517ce3
tsygankov <tsygankov@magora-systems.com>
2019-10-03 11:58:17 +0700

    create files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {SelectorComponent} from './selector/selector.component';
+import {OrganizerComponent} from './organizer/organizer.component';
+import {MomentPipe} from "./shared/moment.pipe";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CalendarComponent,
+        SelectorComponent,
+        OrganizerComponent,
+        MomentPipe
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
